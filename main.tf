3d68036332559ca4c079bda1c6b267b1422e360c
sankaran1 <sankaran.si.1@pg.com>
2021-12-24 13:23:02 -0500

    test

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIA2PSOL46AU2EEVJ7Y"
+  secret_key = "/AaQ7Q5WsIHcsdugUtqjJ2zBTmkgU4KlxNk0Kde7"
+
+  # Allow any 2.x version of the AWS provider
+#  version = "~> 2.0"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+tags = {
+Name = "terraform-example"
+}
+}
